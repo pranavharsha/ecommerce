dc19fdb047ce91654ef539363016767bd557f07b
Pranav Katta <pranav.katta@tigeranalytics.local>
2019-10-10 17:44:43 +0530

    adding build

---
+++ build/precache-manifest.084cc3f3874adc36860272a2d885503c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d2b55f914a176ad7aad9dfb7c37fd2",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "4252b64c6753446b9efa",
+    "url": "/ecommerce/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "e7181a3a9660f8857667",
+    "url": "/ecommerce/static/css/main.3f617599.chunk.css"
+  },
+  {
+    "revision": "4252b64c6753446b9efa",
+    "url": "/ecommerce/static/js/2.9b0f28e4.chunk.js"
+  },
+  {
+    "revision": "e7181a3a9660f8857667",
+    "url": "/ecommerce/static/js/main.0352f235.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  },
+  {
+    "revision": "f3ca8cac0b6978dab6b1f38a5bace3c2",
+    "url": "/ecommerce/static/media/Roboto-Light.f3ca8cac.woff"
+  },
+  {
+    "revision": "71b4038282314812a3fc337e00d7140f",
+    "url": "/ecommerce/static/media/Roboto-Medium.71b40382.woff"
+  },
+  {
+    "revision": "99433f7d92c3a5c1179fa7ebb1ba0a48",
+    "url": "/ecommerce/static/media/Roboto-Regular.99433f7d.woff"
+  }
+]);
